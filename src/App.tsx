1022a0943761d47ae30b0bba406f7cebad6cf9a6
zsolt270 <zsolt270000@gmail.com>
2024-07-31 23:30:35 +0200

    tested the colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import "./App.css";
+
 function App() {
-	return <></>;
+	return (
+		<>
+			<h1>Valami h1</h1>
+			<p className='fw-bold'>
+				Valami szöveg és egy <span>span</span>
+			</p>
+			<div className='darkCarodiv'>ez a div </div>
+
+			<img
+				src='./assets/undraw_coding_re_iv62.svg'
+				alt=''
+			/>
+		</>
+	);
 }
 
 export default App;
