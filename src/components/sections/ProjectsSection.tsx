220f90a14bb36b90918f4f6f0549d903f94e1e2e
zsolt270 <zsolt270000@gmail.com>
2024-08-10 16:44:45 +0200

    carasoul

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -1,19 +1,27 @@
 import { useContext } from "react";
 import { LanguageContext } from "../../services/providers/languageContext";
+import { ThemeContext } from "../../services/providers/themeContext";
 import SectionTitle from "../ui/SectionTitle";
 import Carousel from "../ui/Carousel";
 
 export default function ProjectsSection() {
 	const languageContext = useContext(LanguageContext);
+	const themeContext = useContext(ThemeContext);
 	return (
-		<>
+		<div className=''>
 			<SectionTitle
 				title={
 					languageContext?.language === "HU" ? "Projekteim" : "My Projects"
 				}
 			/>
 			<Carousel />
-			{/* <h3>for more click here</h3> */}
-		</>
+			<a
+				target='_blank'
+				href='https://github.com/zsolt270'
+			>
+				<span className={`text-decoration-underline`}>More Projects Here</span>{" "}
+				{/* <i className='bi bi-box-arrow-up-right'></i> */}
+			</a>
+		</div>
 	);
 }
--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -1,4 +1,5 @@
 import CarouselCard from "./CarouselCard";
+import style from "./css.modules/Carousel.module.css";
 import restcountry from "../../assets/projects/restcountry.png";
 import todoapp from "../../assets/projects/todoapp.png";
 import piano from "../../assets/projects/piano.png";
@@ -8,10 +9,73 @@ import portfolio from "../../assets/projects/portfolio.png";
 export default function Carousel() {
 	return (
 		<>
-			<CarouselCard
-				src={restcountry}
-				title={"Rest Country"}
-			/>
+			<div
+				id='carouselExample'
+				className='carousel slide mb-5'
+			>
+				<div className='carousel-inner'>
+					<div className='carousel-item active'>
+						<CarouselCard
+							src={restcountry}
+							title={"Rest Country"}
+						/>
+					</div>
+					<div className='carousel-item'>
+						<CarouselCard
+							src={todoapp}
+							title={"Todo App"}
+						/>
+					</div>
+					<div className='carousel-item'>
+						<CarouselCard
+							src={piano}
+							title={"Coloured-Piano"}
+						/>
+					</div>
+					<div className='carousel-item'>
+						<CarouselCard
+							src={loopstudios}
+							title={"LoopStudios"}
+						/>
+					</div>
+					<div className='carousel-item'>
+						<CarouselCard
+							src={portfolio}
+							title={"Portfolio"}
+						/>
+					</div>
+				</div>
+				<button
+					className='carousel-control-prev'
+					type='button'
+					data-bs-target='#carouselExample'
+					data-bs-slide='prev'
+				>
+					<span
+						className='carousel-control-prev-icon'
+						aria-hidden='true'
+					></span>
+					<span className='visually-hidden'>Previous</span>
+				</button>
+				<button
+					className='carousel-control-next'
+					type='button'
+					data-bs-target='#carouselExample'
+					data-bs-slide='next'
+				>
+					<span
+						className='carousel-control-next-icon'
+						aria-hidden='true'
+					></span>
+					<span className='visually-hidden'>Next</span>
+				</button>
+			</div>
 		</>
 	);
+}
+{
+	/* <CarouselCard
+src={restcountry}
+title={"Rest Country"}
+/> */
 }
