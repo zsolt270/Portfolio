e283572a6ec28bbef5dd211a404c343094a5eaec
zsolt270 <zsolt270000@gmail.com>
2024-08-10 20:21:53 +0200

    update

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -5,6 +5,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./css.modules/Carousel.css";
+import { NextArrow, PrevArrow } from "./SliderArrow";
 import restcountry from "../../assets/projects/restcountry.png";
 import todoapp from "../../assets/projects/todoapp.png";
 import piano from "../../assets/projects/piano.png";
@@ -22,38 +23,47 @@ export default function Carousel() {
 		autoplay: false,
 		slidesToShow: 3,
 		slidesToScroll: 1,
+		nextArrow: <NextArrow />,
+		prevArrow: <PrevArrow />,
 		responsive: [
+			{
+				breakpoint: 1400,
+				settings: {
+					slidesToShow: 2.8,
+					slidesToScroll: 1,
+					infinite: true,
+				},
+			},
 			{
 				breakpoint: 1200,
 				settings: {
-					slidesToShow: 3,
+					slidesToShow: 2.4,
 					slidesToScroll: 1,
 					infinite: true,
-					dots: false,
 				},
 			},
 			{
 				breakpoint: 1024,
 				settings: {
+					slidesToShow: 2.3,
+					slidesToScroll: 1,
+					infinite: true,
+				},
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 1.8,
+					slidesToScroll: 1,
+					infinite: true,
+				},
+			},
+			{
+				breakpoint: 768,
+				settings: {
 					slidesToShow: 1,
 					slidesToScroll: 1,
 					infinite: true,
-					dots: false,
-				},
-			},
-			{
-				breakpoint: 600,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1,
-					initialSlide: 2,
-				},
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1,
 				},
 			},
 		],
---
+++ src/components/ui/SliderArrow.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useContext } from "react";
+import { ThemeContext } from "../../services/providers/themeContext";
+import styles from "./css.modules/SliderArrow.module.css";
+
+export function NextArrow(props: any) {
+	const themeContext = useContext(ThemeContext);
+	const { className, style, onClick } = props;
+	if (themeContext?.islight) {
+		const nextArrow = className + " " + styles.lightSlickNext;
+		console.log(className);
+		return (
+			<div
+				className={nextArrow}
+				style={{ ...style, display: "block", color: "black" }}
+				onClick={onClick}
+			/>
+		);
+	} else {
+		return (
+			<div
+				className={className}
+				style={{ ...style, display: "block" }}
+				onClick={onClick}
+			/>
+		);
+	}
+}
+
+export function PrevArrow(props: any) {
+	const themeContext = useContext(ThemeContext);
+	const { className, style, onClick } = props;
+	const prevArrow = className + " " + styles.lightSlickPrev;
+	if (themeContext?.islight) {
+		return (
+			<div
+				className={prevArrow}
+				style={{ ...style, display: "block", color: "black" }}
+				onClick={onClick}
+			/>
+		);
+	} else {
+		return (
+			<div
+				className={className}
+				style={{ ...style, display: "block" }}
+				onClick={onClick}
+			/>
+		);
+	}
+}
